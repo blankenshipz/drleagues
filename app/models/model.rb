12a291f4bf637f34c7a88a8f3f1cbe71f1825e38
Zach Blankenship <zach.blankenship@gmail.com>
2019-06-02 20:16:34 -0400

    Add validation for unique model uploads
    I hope this actually works

--- app/models/model.rb
+++ app/models/model.rb
@@ -8,4 +8,16 @@ class Model < ApplicationRecord
   validates :name, uniqueness: { scope: :team_id }
   validates :virtual_score, :physical_score, numericality: true
   validates_presence_of :name, :virtual_score, :team_id
+  validate :unique_file_upload
+
+  def unique_file_upload
+    if file_already_uploaded?
+      errors.add(:model, 'a file with this checksum has been seen before; please upload a new model')
+    end
+  end
+
+  def file_already_uploaded?
+    ActiveStorage::Blob.where(checksum: package.attachment.blob.checksum)
+      .where.not(id: package.attachment.blob.id).exists?
+  end
 end
